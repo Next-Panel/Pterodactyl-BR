3b143f98e1351e65b3eae39bafa78d7a9ba8ed57
Drylian <109999325+drylian@users.noreply.github.com>
2023-02-01 11:09:27 -0400

    Update MailController.php

--- app/Http/Controllers/Admin/Settings/MailController.php
+++ app/Http/Controllers/Admin/Settings/MailController.php
@@ -53,7 +53,7 @@ class MailController extends Controller
     public function update(MailSettingsFormRequest $request): Response
     {
         if ($this->config->get('mail.default') !== 'smtp') {
-            throw new DisplayException('This feature is only available if SMTP is the selected email driver for the Panel.');
+            throw new DisplayException('Este recurso só está disponível se SMTP for o driver de e-mail selecionado para o painel.');
         }
 
         $values = $request->normalize();
