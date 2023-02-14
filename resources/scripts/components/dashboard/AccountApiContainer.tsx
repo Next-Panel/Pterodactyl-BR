d80cb8fd02ec20146ff7cf04d341fb5ac6771838
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-02-14 15:24:41 -0400

    Terceiro Test

--- resources/scripts/components/dashboard/AccountApiContainer.tsx
+++ resources/scripts/components/dashboard/AccountApiContainer.tsx
@@ -8,6 +8,7 @@ import { faKey, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import deleteApiKey from '@/api/account/deleteApiKey';
 import FlashMessageRender from '@/components/FlashMessageRender';
 import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import PageContentBlock from '@/components/elements/PageContentBlock';
 import tw from 'twin.macro';
 import GreyRowBox from '@/components/elements/GreyRowBox';
@@ -74,7 +75,7 @@ export default () => {
                                     <p css={tw`text-sm break-words`}>{key.description}</p>
                                     <p css={tw`text-2xs text-neutral-300 uppercase`}>
                                         Last used:&nbsp;
-                                        {key.lastUsedAt ? format(key.lastUsedAt, 'MMM do, yyyy HH:mm') : 'Never'}
+                                        {key.lastUsedAt ? format(key.lastUsedAt, 'MMM do, yyyy HH:mm') : 'Never', { locale: ptBR }}
                                     </p>
                                 </div>
                                 <p css={tw`text-sm ml-4 hidden md:block`}>
