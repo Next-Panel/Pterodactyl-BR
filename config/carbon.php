0293ff4223f6f6411da20c3c2edb0e77368bb8d7
Drylian <109999325+drylian@users.noreply.github.com>
2023-02-13 23:50:55 -0300

    Agr vai

---
+++ config/carbon.php
@@ -0,0 +1,15 @@
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Carbon Locale e Timezone
+    |--------------------------------------------------------------------------
+    |
+    | criado por Drylian com o objetivo de deixar o tempo em portugues,
+    | este arquivo serve apenas para esse proposito.
+    | 
+    */
+
+    'timezone' => env('APP_TIMEZONE', 'Brazil/Brasilia'),
+    'locale' => env('LINGUA_TIMEZONE', 'pt_BR'),
+    'fallback_locale' => 'pt_BR',
+];
