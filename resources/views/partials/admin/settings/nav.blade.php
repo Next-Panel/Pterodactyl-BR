dfe0e35317be8e0f99feda13f933f0ea304cdc8b
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-02-19 13:30:48 -0400

    Traduções

--- resources/views/partials/admin/settings/nav.blade.php
+++ resources/views/partials/admin/settings/nav.blade.php
@@ -6,9 +6,9 @@
         <div class="col-xs-12">
             <div class="nav-tabs-custom nav-tabs-floating">
                 <ul class="nav nav-tabs">
-                    <li @if($activeTab === 'basic')class="active"@endif><a href="{{ route('admin.settings') }}">General</a></li>
-                    <li @if($activeTab === 'mail')class="active"@endif><a href="{{ route('admin.settings.mail') }}">Mail</a></li>
-                    <li @if($activeTab === 'advanced')class="active"@endif><a href="{{ route('admin.settings.advanced') }}">Advanced</a></li>
+                    <li @if($activeTab === 'basic')class="active"@endif><a href="{{ route('admin.settings') }}">Geral</a></li>
+                    <li @if($activeTab === 'mail')class="active"@endif><a href="{{ route('admin.settings.mail') }}">E-mail</a></li>
+                    <li @if($activeTab === 'advanced')class="active"@endif><a href="{{ route('admin.settings.advanced') }}">Avançado</a></li>
                 </ul>
             </div>
         </div>
