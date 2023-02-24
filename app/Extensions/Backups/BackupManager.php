63d185056169a2ec46811527db5d3084d99d19f8
Drylian <109999325+drylian@users.noreply.github.com>
2023-02-24 14:19:18 -0300

    Att BackupManager

--- app/Extensions/Backups/BackupManager.php
+++ app/Extensions/Backups/BackupManager.php
@@ -86,7 +86,7 @@ class BackupManager
             return $instance;
         }
 
-        throw new \InvalidArgumentException("Adapter [$adapter] is not supported.");
+        throw new \InvalidArgumentException("Adaptador [$adapter] não é suportado.");
     }
 
     /**
