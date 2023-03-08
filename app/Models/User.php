8a5d777eb301eca6d4ac3699ca6a23892ea659b0
Drylian <109999325+drylian@users.noreply.github.com>
2023-03-08 10:52:35 -0300

    pt < en

--- app/Models/User.php
+++ app/Models/User.php
@@ -148,7 +148,7 @@ class User extends Model implements
     protected $attributes = [
         'external_id' => null,
         'root_admin' => false,
-        'language' => 'pt',
+        'language' => 'en',
         'use_totp' => false,
         'totp_secret' => null,
     ];
--- resources/lang/pt/dashboard/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'search' => 'Busca de servidores...',
-    'no_matches' => 'Não foram encontrados servidores que correspondessem aos critérios de busca fornecidos.',
-    'cpu_title' => 'CPU',
-    'memory_title' => 'Memória.',
-];
