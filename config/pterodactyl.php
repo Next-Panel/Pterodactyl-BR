01888013c9b215f5608871a870cf712648562529
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-03-11 22:55:08 -0400

    👀 Trocar URL da API

--- config/pterodactyl.php
+++ config/pterodactyl.php
@@ -92,7 +92,7 @@ return [
 
     'cdn' => [
         'cache_time' => 60,
-        'url' => 'https://raw.githubusercontent.com/Next-Panel/Pterodactyl-API/main/status.json',
+        'url' => 'https://raw.githubusercontent.com/Next-Panel/Nextpanel-API/master/version/pterodactyl.json',
     ],
 
     /*
