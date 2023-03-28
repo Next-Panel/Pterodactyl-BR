5bc265542d8aa34b98d6c84dc9eec51aa71777fe
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-03-28 13:45:25 -0400

    Update 2023_03_28_132515_add_sftp_ip_field_node_table.php

--- database/migrations/2023_03_28_132515_add_sftp_ip_field_node_table.php
+++ database/migrations/2023_03_28_132515_add_sftp_ip_field_node_table.php
@@ -1,7 +1,7 @@
 <?php
 
-+use Illuminate\Support\Facades\Schema;
-+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 return new class () extends Migration {
