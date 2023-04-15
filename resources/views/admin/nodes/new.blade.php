27357f89833cae831badeea362e01e35a4ab2e62
Drylian <109999325+drylian@users.noreply.github.com>
2023-04-14 21:25:56 -0300

    atualização visual na criação de node

--- resources/views/admin/nodes/new.blade.php
+++ resources/views/admin/nodes/new.blade.php
@@ -55,9 +55,18 @@
                         <p class="text-muted small">Ao definir um Node como<code>privado</code>, você estará negando a capacidade de implantar automaticamente nesse node.
                     </div>
                     <div class="form-group">
-                        <label for="pFQDN" class="form-label">FQDN</label>
+                        <label for="pFQDN" class="form-label">FQDN Wings</label>
                         <input type="text" name="fqdn" id="pFQDN" class="form-control" value="{{ old('fqdn') }}"/>
                         <p class="text-muted small">Insira o nome de domínio (por exemplo,<code> node.example.com</code>) a ser usado para se conectar ao daemon. Um endereço IP pode ser usado <em>somente</em> se você não estiver usando SSL para esse node.</p>
+                    </div>
+                    <div class="form-group">
+                        <label for="daemonSFTPIP" class="control-label">FQDN SFTP (Opcional)</label>
+                        <div>
+                        <input type="text" name="daemonSFTPIP" class="form-control" id="pDaemonSFTPIP"/>
+                        </div>
+                        <p class="text-muted"><small>Insira o nome do domínio SFTP ou ip (por exemplo, <code>sftp.example.com</code> ou <code>123.456.789.123</code>) a ser usado para se conectar ao sftp do daemon.
+                                <a tabindex="0" data-toggle="popover" data-trigger="focus" title="Por que eu preciso de uma FQDN para o SFTP?" data-content="Ao utilizar um SFTP separado, é possível configurar um servidor Wings atrás do Proxy do Cloudflare. Isso pode melhorar a segurança e a eficiência do servidor.">Por quê?</a>
+                            </small></p>
                     </div>
                     <div class="form-group">
                         <label class="form-label">Comunicar por SSL</label>
@@ -144,22 +153,21 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group col-md-4">
-                            <label for="pDaemonListen" class="form-label">Porta do Daemon</label>
-                            <input type="text" name="daemonListen" class="form-control" id="pDaemonListen" value="8080" />
-                        </div>
-                        <div class="form-group col-md-4">
-                            <label for="pDaemonSFTPIP" class="form-label">IP SFTP do Daemon</label>
-                            <input type="text" name="daemonSFTPIP" class="form-control" id="pDaemonSFTPIP"/>
-                        </div>
-                        <div class="form-group col-md-4">
-                            <label for="pDaemonSFTP" class="form-label">Porta SFTP do Daemon</label>
-                            <input type="text" name="daemonSFTP" class="form-control" id="pDaemonSFTP" value="2022" />
-                        </div>
-                        <div class="col-md-12">
+                    <div class="form-group col-md-6">
+                                <label for="daemonListen" class="control-label"><span class="label label-warning"><i class="fa fa-power-off"></i></span>Porta do Daemon</label>
+                                <div>
+                                <input type="text" name="daemonListen" class="form-control" id="pDaemonListen" value="8080" />
+                                </div>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="daemonSFTP" class="control-label"><span class="label label-warning"><i class="fa fa-power-off"></i></span>Porta SFTP do Daemon</label>
+                                <div>
+                                <input type="text" name="daemonSFTP" class="form-control" id="pDaemonSFTP" value="2022" />
+                                </div>
+                    </div>
+                    <div class="col-md-12">
                             <p class="text-muted small">O daemon executa seu próprio contêiner de gerenciamento SFTP e não utiliza o processo SSHd no servidor físico principal. <Strong>Não utilize a mesma porta que você designou para o processo SSH do seu servidor físico.</strong> Se você estiver executando o daemon atrás do CloudFlare&reg; você deve configurar o porta daemon para <code>8443</code> para permitir a proxy de websocket sobre SSL.</p>
                         </div>
-                    </div>
                 </div>
                 <div class="box-footer">
                     {!! csrf_field() !!}
